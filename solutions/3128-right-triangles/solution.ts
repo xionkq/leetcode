cc3db127f192fd9a948234568c2a288d3d7a4be7
xionkq <3030199850@qq.com>
2024-08-02 15:44:56 +0800

    feat: solve 3128-right-triangles

---
+++ solutions/3128-right-triangles/solution.ts
@@ -0,0 +1,33 @@
+/**
+ * 思路：遍历矩阵，当遍历到 1 时，计算以该点为直角点的直角三角形个数
+ * 计算可通过（该行节点值为 1 的个数 - 1） * （该列节点值为 1 的个数 - 1）
+ * 时间复杂度：O(m * n)
+ * 空间复杂度：O(m * n)
+ */
+
+function numberOfRightTriangles(grid: number[][]): number {
+  const m = grid.length
+  const n = grid[0].length
+  const countMap = new Map<string, number>()
+  let res = 0
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        countMap.set(`row${i}`, (countMap.get(`row${i}`) || 0) + 1)
+        countMap.set(`col${j}`, (countMap.get(`col${j}`) || 0) + 1)
+      }
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        const rowCount = countMap.get(`row${i}`)! - 1
+        const colCount = countMap.get(`col${j}`)! - 1
+        res += rowCount * colCount
+      }
+    }
+  }
+
+  return res
+};
